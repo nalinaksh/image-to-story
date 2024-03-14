b2f683f1b9fda9c47653bf2ccb58b09e7ee05529
Nalinaksh Gaur <nalinaksh@gmail.com>
2024-03-14 16:12:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 from transformers import BlipForConditionalGeneration, AutoProcessor
-# from transformers import pipeline
-# from huggingface_hub import snapshot_download
 from openai import OpenAI
 import streamlit as st
 from PIL import Image
@@ -8,11 +6,6 @@ import os
 
 model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
 processor = AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
-
-# snapshot_download(repo_id="Salesforce/blip-image-captioning-base", local_dir="Salesforce")
-
-# pipe = pipeline("image-to-text", model="Salesforce")
-
 client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
 
 def caption_to_story(image_caption):
@@ -31,8 +24,6 @@ def image_to_text(image):
     inputs = processor(image,return_tensors="pt")
     out = model.generate(**inputs)
     image_caption = processor.decode(out[0], skip_special_tokens=True)
-    # pipeline_output = pipe(image)
-    # story = caption_to_story(pipeline_output[0]['generated_text'])
     story = caption_to_story(image_caption)
     return story
 
@@ -41,7 +32,8 @@ uploaded_file = st.file_uploader("Choose a file", type=['png', 'jpg'])
 
 # If user attempts to upload a file.
 if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image")
+  image = Image.open(uploaded_file)
+  st.image(image, caption="Uploaded Image")
+  with st.spinner('Generating short story...'):
     story = image_to_text(image)
     st.write(story)
